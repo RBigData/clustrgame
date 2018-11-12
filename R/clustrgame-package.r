200b819e8d0b6f461a8fb2acbdc0e6b35b713347
wrathematics <wrathematics@gmail.com>
2018-11-11 19:43:25 -0500

    first

---
+++ R/clustrgame-package.r
@@ -0,0 +1,12 @@
+#' clustrgame
+#' 
+#' TODO
+#' 
+#' @import pbdMPI
+#' @import kazaam
+#' 
+#' @name clustrgame-package
+#' @docType package
+#' @references Programming with Big Data in R Website: \url{https://pbdr.org/}
+#' @keywords Package
+NULL

b91b117ec5d9ed3d9faf2d452b23a7328e2f3322
wrathematics <wrathematics@gmail.com>
2019-01-15 13:36:36 -0500

    add structs

---
+++ src/cpu/kmeans.h
@@ -0,0 +1,43 @@
+#ifndef KMEANS_H_
+#define KMEANS_H_
+
+
+#include <mpi.h>
+#include <stdint.h>
+
+#include "types.h"
+
+#define NROWS(x) (x->nrows)
+#define NCOLS(x) (x->ncols)
+#define NROWS_LOCAL(x) (x->nrows_local)
+#define NCOLS_LOCAL(x) (x->ncols_local)
+#define DATA(x) (x->data)
+#define COMM(x) (x->comm)
+
+typedef struct shaq
+{
+  len_t nrows;
+  len_t ncols;
+  int nrows_local;
+  int ncols_local; // same as ncols
+  double *restrict data;
+  MPI_Comm comm;
+} shaq;
+
+typedef struct kmeans_vals
+{
+  double *restrict centers;
+  int *restrict labels;
+  int *restrict nlabels;
+} kmeans_vals;
+
+typedef struct kmeans_opts
+{
+  int k;
+  int maxiter;
+  int seed;
+  int zero_indexed;
+} kmeans_opts;
+
+
+#endif

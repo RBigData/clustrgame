1d8d934a29cb672839434a14ea073d493f5b470c
Drew Schmidt <wrathematics@gmail.com>
2019-03-21 09:05:29 -0400

    skeleton for internal float support

--- src/kmeans.cpp
+++ src/kmeans.cpp
@@ -11,11 +11,9 @@
 
 extern "C" SEXP R_kmeans(SEXP data, SEXP m, SEXP k_, SEXP maxiter, SEXP comm_)
 {
+  int check;
   SEXP ret, ret_names;
   SEXP ret_centers, ret_labels, ret_niters;
-  shaq<double> x;
-  kmeans_vals<double> km;
-  kmeans_opts opts;
   
   MPI_Comm comm = get_mpi_comm_from_Robj(comm_);
   
@@ -23,27 +21,35 @@ extern "C" SEXP R_kmeans(SEXP data, SEXP m, SEXP k_, SEXP maxiter, SEXP comm_)
   const int n = ncols(data);
   const int k = INTEGER(k_)[0];
   
-  PROTECT(ret_centers = allocMatrix(REALSXP, n, k));
-  PROTECT(ret_labels = allocVector(INTSXP, m_local));
-  PROTECT(ret_niters = allocVector(INTSXP, 1));
-  
-  PROTECT(ret = allocVector(VECSXP, 3));
-  PROTECT(ret_names = allocVector(STRSXP, 3));
-  
-  x.nrows = INTEGER(m)[0];
-  x.ncols = n;
-  x.nrows_local = m_local;
-  x.data = REAL(data);
-  x.comm = comm;
-  
-  km.centers = REAL(ret_centers);
-  km.labels = INTEGER(ret_labels);
-  
+  kmeans_opts opts;
   opts.k = k;
   opts.maxiter = INTEGER(maxiter)[0];
   opts.zero_indexed = 0;
   
-  int check = kmeans(&x, &km, &opts);
+  PROTECT(ret_labels = allocVector(INTSXP, m_local));
+  PROTECT(ret_niters = allocVector(INTSXP, 1));
+  
+  if (TYPEOF(data) == REALSXP)
+  {
+    PROTECT(ret_centers = allocMatrix(REALSXP, n, k));
+    
+    shaq<double> x;
+    x.nrows = INTEGER(m)[0];
+    x.ncols = n;
+    x.nrows_local = m_local;
+    x.data = REAL(data);
+    x.comm = comm;
+    
+    kmeans_vals<double> km;
+    km.centers = REAL(ret_centers);
+    km.labels = INTEGER(ret_labels);
+    
+    check = kmeans(&x, &km, &opts);
+  }
+  else if (TYPEOF(data) == INTSXP)
+  {
+    error("TODO");
+  }
   
   if (check == ERROR_MPI)
     R_err_mpi(check, comm);
@@ -51,6 +57,9 @@ extern "C" SEXP R_kmeans(SEXP data, SEXP m, SEXP k_, SEXP maxiter, SEXP comm_)
     R_err_malloc(comm);
   
   INTEGER(ret_niters)[0] = check;
+  
+  PROTECT(ret = allocVector(VECSXP, 3));
+  PROTECT(ret_names = allocVector(STRSXP, 3));
   
   SET_VECTOR_ELT(ret, 0, ret_centers);
   SET_VECTOR_ELT(ret, 1, ret_labels);

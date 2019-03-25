e5b4dda68f9a548cde7eee149d0c008d00d41271
wrathematics <wrathematics@gmail.com>
2019-03-25 14:31:25 -0400

    avoid double init

--- R/zzz.r
+++ R/zzz.r
@@ -1,6 +1,10 @@
 #' @useDynLib clustrgame R_clustrgame_init
 .onLoad = function(libname, pkgname)
 {
+  s = search()
+  if ("package:dimrgame" %in% s || "package:glmrgame" %in% s)
+    return(invisible())
+  
   comm_ptr = pbdMPI::get.mpi.comm.ptr(.pbd_env$SPMD.CT$comm)
   .Call(R_clustrgame_init, comm_ptr)
   
